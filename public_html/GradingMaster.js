ee647750d85bcd40a2d299876fa40a0ce9ff7fa2
kiahb <kiahb@Kiahs-Laptop.niacc.edu>
2020-10-29 17:32:41 -0500

    added code

--- public_html/GradingMaster.js
+++ public_html/GradingMaster.js
@@ -15,4 +15,39 @@
 
 document.write("<p>Grade calculation program</p>");
 
+let grades = [];
+let total = 0;
+let score = 0;
+let counter = 1;
+while (counter <= 5){
+    score = prompt("Enter the score: ");
+    score = Number(score);
+    total = total + score;
+    grades.push(score);
+    counter = counter + 1;
+}
 
+let avg = total/5;
+document.write("The grade % is " + avg);
+
+if (avg < 60){
+    document.write("<br>F");
+} 
+if (avg >= 60){
+    if (avg <=69){
+        document.write("<br>D");
+    }
+}
+if (avg >= 70){
+    if (avg <= 79){
+        document.write("<br>C");
+    }
+}
+if (avg >= 80){
+    if (avg <= 89){
+        document.write("<br>B");
+    }
+}
+if (avg >= 90){
+    document.write("<br>A");
+}
